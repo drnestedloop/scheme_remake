ae911263be5126919e01af601d252d319c30125e
Tristan Staschik <tristan.staschik@gmail.com>
2022-04-29 00:27:05 -0700

    initial commit; parser currently supports lambda expressions, define expressions, variable lookup, and linked lists

---
+++ builtin_items.py
@@ -0,0 +1,83 @@
+class Frame:
+    def __init__(self, parent=None) -> None:
+        self.parent = parent
+        self.names = {}
+    def lookup(self, name: str):
+        if name in self.names:
+            return self.names[name]
+        else:
+            if(not self.parent is None):
+                return self.parent.lookup(name)
+            else:
+                assert False, f"Error unbound variable name {name}"
+    def define(self, name: str, value: any):
+        self.names[name] = value
+    def make_child(self):
+        return Frame(parent=self)
+class Comment:
+    def __init__(self, message='') -> None:
+        self.message = message
+class SchemeList:
+    def __init__(self, first, rest) -> None:
+        assert not rest is None, "Error: List must have two parts. You can always use nil!"
+        self.first = first
+        self.rest = rest
+    def __str__(self) -> str:
+        stringified_list = "(list"
+        current_list = self
+        while not current_list is nil:
+            stringified_list += " " + str(current_list.first)
+            current_list = current_list.rest
+        return stringified_list + ")"
+class Nil: #made to create the Nil object
+    def __init__(self) -> None:
+        pass
+#define some important global variables
+GLOBAL_FRAME = Frame()
+nil = Nil()
+
+#make it easy to add functions to the global frame
+def define_in_global_frame(name:str):
+    return lambda value: GLOBAL_FRAME.define(name, value)
+
+@define_in_global_frame("+")
+def scheme_add(args):
+    total = 0
+    for arg in args:
+        total += arg
+    return total
+@define_in_global_frame("-")
+def scheme_subtract(args):
+    i = args[0]
+    for arg in args[1:]:
+        i -= arg
+    return i
+@define_in_global_frame("power")
+def scheme_exponentiate(args):
+    assert len(args) == 2, "Error: Incorrect number of arguments passed to built-in function power"
+    return args[0] ** args[1]
+@define_in_global_frame("print")
+def scheme_print(args):
+    for arg in args:
+        #ensure that True and False print as #t and #f
+        if(arg == True): arg = "#t"
+        if(arg == False): arg = "#f"
+        print(">> ", arg)
+@define_in_global_frame("car")
+def scheme_car(args):
+    assert len(args) == 1, "Error: Incorrect number of arguments passed to built-in function car"
+    return args[0].first
+@define_in_global_frame("cdr")
+def scheme_cdr(args):
+    assert len(args) == 1, "Error: Incorrect number of arguments passed to built-in function cdr"
+    return args[0].rest
+@define_in_global_frame("list")
+def scheme_list(args):
+    if(len(args) == 1):
+        return SchemeList(args[0], nil)
+    #For some reason the name 'scheme_list' is set to None in the current environment, I got around this by referencing the function by looking it up in GLOBAL_FRAME
+    return SchemeList(args[0], GLOBAL_FRAME.lookup('list')(args[1:]))
+
+#add some important constants to the global frame
+define_in_global_frame("#t")(True)
+define_in_global_frame("#f")(False)
---
+++ parse.py
@@ -0,0 +1,72 @@
+import lark
+from builtin_items import *
+
+grammar = open("grammarfile.lark", "r")
+grammar = grammar.read()
+
+parser = lark.Lark(grammar)
+
+
+def do_lambda_expression(parts:list, env):
+    formals = parts[0]
+    assert formals.data[:] == 'call', "Error: Malformed Lambda Expression"
+    formals = formals.children
+    body_expressions = parts[1:]
+    lambda_frame = env.make_child()
+    def lambda_call(args):
+        assert len(args)  == len(formals), "Error: incorrect number of arguments passed to lambda"
+        #bind args to their names 
+        for i in range(len(formals)):
+            lambda_frame.define(formals[i][:], args[i])
+        for expr in body_expressions[:-1]:
+            evaluate(expr, lambda_frame)
+        return evaluate(body_expressions[-1], lambda_frame)
+    return lambda_call
+
+def do_define(AST, env):
+    if(len(AST.children) != 2):
+        assert False, "Error: malformed define statement"
+    #evaluate value to be saved
+    value = evaluate(AST.children[1], env)
+    return env.define(AST.children[0][:], value)
+
+def do_call(AST, env):
+    if(type(AST.children[0]) == lark.tree.Tree):
+        opperator = evaluate(AST.children[0], env)
+    else:
+        fname = AST.children[0][:]
+        opperator = env.lookup(fname)
+    args = AST.children[1:]
+    evaled_args = []
+    for arg in args:
+        evaled = evaluate(arg, env)
+        if(type(evaled) == Comment):
+            pass
+        else:
+            evaled_args.append(evaled)
+    return opperator(evaled_args)
+
+
+def evaluate(AST, env): #TODO rename parameter from AST since the input is not always an AST
+    if(type(AST) == lark.tree.Tree):
+        if(AST.data[:] == 'lambda_expr'):
+            return do_lambda_expression(AST.children, env)
+        elif(AST.data[:] == 'define'):
+            return do_define(AST, env)
+        elif(AST.data[:] == 'call'):
+            return do_call(AST, env)
+        elif(AST.data[:] == "multi_expr"):
+            for expr in AST.children:
+                evaluate(expr, env)
+        elif(AST.data[:] == "comment"):
+            return Comment(AST.children[0][:]) #do nothing, it's a comment
+    elif(type(AST) == lark.lexer.Token):
+        if(AST.type == 'NUMBER'):
+            try:
+                intified_number = int(AST[:])
+            except ValueError:
+                intified_number = None
+            floatified_number = float(AST[:])
+            return intified_number if floatified_number == intified_number else floatified_number
+        else:
+            return env.lookup(AST[:])
